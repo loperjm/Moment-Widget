396c9cbada71168d033175ca6c5ae03449efa539
LastLoper <ys.mobility@gmail.com>
2022-11-06 18:18:44 +0900

    first commit

---
+++ moment-widget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moment-widget
+//
+//  Created by Walter J on 2022/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
